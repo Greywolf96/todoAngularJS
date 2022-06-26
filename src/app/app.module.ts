5104aeba6aff29902ab8fdd74738f41acc8d38c7
Sam <limsiansiing@gmail.com>
2022-06-27 05:43:00 +0800

    component features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
